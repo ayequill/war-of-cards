06b96e4c16a2f0c3f6279f7f6121d569a92c7371
ayequill <ayequillgh@gmail.com>
2023-01-01 09:30:22 +0000

    initial upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let deckId
+let str
+function fetchApi() {
+    fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(res => res.json())
+    .then(data => {
+        return deckId = data.deck_id
+    })
+}
+
+function drawCard() {
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+.then(res => res.json())
+.then(deck => {
+    console.log(deck.cards);
+    document.getElementById('cards').innerHTML =`<img src='${deck.cards[0].image}'>
+                                                 <img src='${deck.cards[1].image}'>`
+    })
+}
+
+
+function renderCards() {
+
+    document.getElementById('cards').innerHTML = str
+    
+}
+
+
+document.getElementById('btn').addEventListener('click', fetchApi)
+
+document.getElementById('drawBtn').addEventListener('click', ()=>{
+
+    if(deckId){
+        drawCard()
+    }else {
+        fetchApi ()
+        drawCard ()
+    }
+
+})
+
