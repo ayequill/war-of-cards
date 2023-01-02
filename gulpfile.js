e06e34f65b44238ad093330087e1cb6b4c01a005
ayequill <ayequillgh@gmail.com>
2023-01-02 22:35:20 +0000

    added features

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 //  declare gulp methods
-const {src, dest, series, watch } = require('gulp') 
+const { src, dest, series, watch } = require("gulp");
 
 // !styles
 // declaring gulp plugins for the styles
-const scss = require('gulp-sass')(require('sass'))
-const autoprefixer = require('gulp-autoprefixer')
-const cssMin = require('gulp-clean-css')
-const unCss = require('gulp-uncss')
+const scss = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const cssMin = require("gulp-clean-css");
+const unCss = require("gulp-uncss");
 
-function styles(){
-    // first return my source loc and chain it with the gulp plugins 
-    return src('./src/scss/**/*.scss')
-        .pipe ( scss() )
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe( cssMin() )
-        // .pipe(unCss({
-        //     html:'index.html'
-        // }))
-        // declare save loc for my processed files
-        .pipe( dest('./dist/styles/') )
+function styles() {
+  // first return my source loc and chain it with the gulp plugins
+  return (
+    src("./src/scss/**/*.scss")
+      .pipe(scss())
+      .pipe(autoprefixer("last 2 versions"))
+      .pipe(cssMin())
+      // .pipe(unCss({
+      //     html:'index.html'
+      // }))
+      // declare save loc for my processed files
+      .pipe(dest("./dist/styles/"))
+  );
 }
 
 // !scripts
 //* declaring gulp plugins for the scripts
 
-const jsMin = require('gulp-terser')
+const jsMin = require("gulp-terser");
 
-function scripts(){
-    return src('./src/js/**/*.js')
-        .pipe( jsMin() )
-        .pipe(dest('./dist/js'))
+function scripts() {
+  return src("./src/js/**/*.js").pipe(jsMin()).pipe(dest("./dist/js"));
 }
 
 //!watchTask
 // used the series method to invoke two my styles and script function
-function watchTask(){
-    watch(
-        /*the watch func takes two param. 
+function watchTask() {
+  watch(
+    /*the watch func takes two param. 
         the locations of the task you want to monitor for changes
         */
-        ['./src/scss/**/*.scss', './src/js/**/*.js'],
-        series(styles,scripts)
-    )
+    ["./src/scss/**/*.scss", "./src/js/**/*.js"],
+    series(styles, scripts)
+  );
 }
 
-// exported the 
-exports.default = series(styles, scripts , watchTask)
+// exported the
+exports.default = series(styles, scripts, watchTask);
--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,109 @@
-let deckId
-let str
-function fetchApi() {
-    fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res => res.json())
-    .then(data => {
-        return deckId = data.deck_id
-    })
+let deckId;
+let str;
+const cardsHolder = document.getElementById("cards");
+const newGameBtn = document.getElementById("btn");
+const drawBtn = document.getElementById("drawBtn");
+const scoreBoard = document.querySelector("h2");
+const remainingCardsContainer = document.querySelector("h3");
+const scores = document.querySelectorAll("p");
+let playerScore = 0;
+let cpuScore = 0;
+console.log(scores);
+
+async function fetchApi() {
+  // Fetch new game from API
+  const response = await fetch(
+    "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  );
+  const data = await response.json();
+
+  // render remaining card at the start of the game
+  remainingCardsContainer.textContent = `Remaining cards: ${data.remaining}`;
+
+  //   return deck id to keep track of game
+  return (deckId = data.deck_id);
 }
 
-function drawCard() {
-    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-.then(res => res.json())
-.then(deck => {
-    console.log(deck.cards);
-    /* TODO Render each image in its own div */
+async function drawCard() {
+  const response = await fetch(
+    `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+  );
+  const deck = await response.json();
+  //   render remaining cards and update it
+  remainingCardsContainer.textContent = `Remaining cards: ${deck.remaining}`;
 
-    document.getElementById('cards').innerHTML =`<img src='${deck.cards[0].image}'>
-                                                 <img src='${deck.cards[1].image}
-                                                 '>
-                                                 <p>Computer:</p>
-                                                 <p>Me:</p>`
-    })
+  // render each card image
+  cardsHolder.children[0].innerHTML = `<img class="animate__animated animate__flipInX" src='${deck.cards[0].image}'>`;
+  cardsHolder.children[1].innerHTML = `<img class="animate__animated animate__flipInX" src='${deck.cards[1].image}'>`;
+
+  //   disable border after rendering images
+  cardsHolder.children[0].style.border = "none";
+  cardsHolder.children[1].style.border = "none";
+
+  // check who won the round and render
+  scoreBoard.textContent = determineCardWinner(deck.cards[0], deck.cards[1]);
+
+  // if there are no cards disable draw button and render who won the game
+  if (deck.remaining === 0) {
+    if (playerScore < cpuScore) {
+      scoreBoard.textContent = "CPU Won";
+    } else if (cpuScore < playerScore) {
+      scoreBoard.textContent = "Player Won";
+    } else scoreBoard.textContent = "war";
+    drawBtn.disabled = true;
+  }
+
+  // up each rounds score
+  scores[0].textContent = `Computer: ${cpuScore}`;
+  scores[1].textContent = `Me: ${playerScore}`;
 }
 
+newGameBtn.addEventListener("click", () => {
+  fetchApi();
+  playerScore = 0;
+  cpuScore = 0;
+  scores.forEach((score) => (score.textContent = ""));
+});
 
-function renderCards() {
+drawBtn.addEventListener("click", () => {
+  if (deckId) {
+    drawCard();
+  } else {
+    fetchApi();
+    drawCard();
+  }
+});
 
-    document.getElementById('cards').innerHTML = str
-    
+function determineCardWinner(firstCard, secondCard) {
+  //  an array to use as a counter for my cards since not all of them are numbers
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+
+  const card1ValueIndex = valueOptions.indexOf(firstCard.value);
+  const card2ValueIndex = valueOptions.indexOf(secondCard.value);
+  console.log(card1ValueIndex);
+  console.log(card2ValueIndex);
+
+  if (card1ValueIndex < card2ValueIndex) {
+    console.log("secondCard won");
+    playerScore++;
+    return `You won!`;
+  } else if (card1ValueIndex > card2ValueIndex) {
+    console.log("firstCard won");
+    cpuScore++;
+    return `Computer won!`;
+  } else return `War!`;
 }
-
-
-document.getElementById('btn').addEventListener('click', fetchApi)
-
-document.getElementById('drawBtn').addEventListener('click', ()=>{
-
-    if(deckId){
-        drawCard()
-    }else {
-        fetchApi ()
-        drawCard ()
-    }
-
-})
-
